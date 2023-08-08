2f17f82a2ed959bb6c6c18d9a98a1d0b2ad976dc
Kevin Molina <kevinmolina.dev@gmail.com>
2023-08-08 11:47:43 -0400

    Introducción a JPA y Hibernate en Java

---
+++ Base de datos III/Java/db_practica1/src/main/java/com/kevin/db_practica1/DbPractica1Application.java
@@ -0,0 +1,13 @@
+package com.kevin.db_practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPractica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPractica1Application.class, args);
+	}
+
+}
